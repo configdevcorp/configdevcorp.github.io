7289ba1a809e736da272df83ec443921d8f9f01c
configdevcorp <55986170+configdevcorp@users.noreply.github.com>
2019-10-06 19:25:17 +0700

    Add files via upload

---
+++ projects/configdev/js/common.js
@@ -0,0 +1,112 @@
+$(function() {
+
+
+
+// initialize scrollspy
+// $('body').scrollspy({ target: '.dotted-scrollspy' });
+
+
+
+// $(".button-collapse").sideNav();
+// var el = document.querySelector('.custom-scrollbar');
+// var ps = new PerfectScrollbar(el);
+
+
+// $(".button-collapse").sideNav();
+// var el = document.querySelector('.custom-scrollbar');
+// var ps = new PerfectScrollbar(el);
+
+
+
+
+// $('.btn-close-sidenav').on('click', '.btn-close', function () {
+
+//       $('#sidenav-overlay').remove();
+//       $('#slide-out').attr('style','transform: translateX(-100%);transition:  all 200ms cubic-bezier(0.25, 0.46, 0.45, 0.94)');
+//     });
+
+
+
+    // SideNav Button Initialization
+    // $(".button-collapse").sideNav();
+    // // SideNav Scrollbar Initialization
+    // var el = document.querySelector('.custom-scrollbar');
+    // var ps = new PerfectScrollbar(el);
+
+
+    // $('.btn-close-wrapper').on('click', '.btn-close', function () {
+
+    //   $('#sidenav-overlay').remove();
+    //   $('#slide-out').attr('style','transform: translateX(-100%);transition:  all 200ms cubic-bezier(0.25, 0.46, 0.45, 0.94)');
+    // });
+
+
+
+
+
+
+
+new WOW().init();
+
+
+// $('.carousel').carousel();
+
+
+/*---Resize Height---*/
+  // function height_Detect() {
+  //   $(".main_header").css("height", $(window).height());
+  // };
+  // height_Detect();
+  // $(window).resize(function() {
+  //   height_Detect();
+  // });
+  /*---End Resize Height---*/
+
+
+
+
+
+  /* Start .main-faq */
+
+ // $("a.collapsed").click(function(){
+ //      $(this).find(".btn:contains('answer')").toggleClass("collapsed");
+ //  });
+
+ /* End .main-faq */
+
+
+
+  // new WOW().init();
+
+
+  // $("input, select, textarea").jqBootstrapValidation();
+  // $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+  
+
+
+  // $("#modal-phone").mask("+7 (999) 999-99-99");
+
+  //   $("#action-phone").mask("+7 (999) 999-99-99");
+
+  // $("#brief-phone").mask("+7 (999) 999-99-99");
+
+
+
+
+});
+
+
+
+// $(".main-testimonials .fancybox").fancybox();
+
+
+
+
+
+/*---Preloader---*/
+// $(window).on('load', function() { 
+//   $(".loader_inner").fadeOut(); 
+//   $(".loader").delay(400).fadeOut("slow"); 
+// });
+
+/*---End Preloader---*/
